d0d2ef694435bef7ab1284e961e31e846856c11f
unknown <sander.semmel@gmail.com>
2018-08-09 22:10:39 +0300

    added links to partnerimages

--- src/FooterComponent.js
+++ src/FooterComponent.js
@@ -11,10 +11,11 @@ class FooterComponent extends React.Component {
             <React.Fragment>
                 <div className="footerDiv">
                     <h3>Talo Haltiatar</h3>
-                    <img className="partnerImage" src={ImageAssets.visitpori}></img>
-                    <img className="partnerImage" src={ImageAssets.karjaranta21}></img>
-                    <img className="partnerImage" src={ImageAssets.samklogo}></img>
-                    <img className="partnerImage" src={ImageAssets.paulalogo}></img>
+                    <a href="https://www.visitpori.fi/"><img className="partnerImage" src={ImageAssets.visitpori}></img></a>
+                    <a href="http://www.karjaranta21.fi/"><img className="partnerImage" src={ImageAssets.karjaranta21}></img></a>
+                    <a href="https://www.samk.fi/en/"><img className="partnerImage" src={ImageAssets.samklogo}></img></a>
+                    <a href="https://www.paulahellberg.fi/"><img className="partnerImage" src={ImageAssets.paulalogo}></img></a>
+                    
                         <div className="footerLow">
                             <div className="year">
                                 <p className="yearText">2018</p>
